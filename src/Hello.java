8e67025dbee6b11879d1ed0a8050aa3a551bd4ee
sai-shanmukhi <sai.shannu.94@gmail.com>
2017-02-24 11:27:55 -0500

    second commit

--- src/Hello.java
+++ src/Hello.java
@@ -31,7 +31,7 @@ public class Hello extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
 		 response.setContentType("text/html");
 	        PrintWriter printWriter  = response.getWriter();
 	        //printWriter.println("<h1>Hello World!</h1>");
